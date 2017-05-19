3b259fd5b1bbd621ce5331e3a42483d31fcf5361
T- <T-@PC251>
2017-05-18 19:36:17 -0500

    clase del jueves 18

--- src/alfredo/vargas/AlfredoVargas.java
+++ src/alfredo/vargas/AlfredoVargas.java
@@ -16,6 +16,17 @@ public class AlfredoVargas {
      */
     public static void main(String[] args) {
         System.out.println("alfredo");
+        
+        //probaremos nuestra solucion
+        //1-crear 1 usuario hipotetico
+        Usuario u=new Usuario();
+        //2- le asigno valores
+        u.altura=1.69f;
+        u.peso=86f;
+        //3-generamos el modelo IMC
+        Imc imc=new Imc();
+      
+        System.out.println("el valor de imc es "+imc.calcular(u));
     }
     
 }
---
+++ src/alfredo/vargas/Imc.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alfredo.vargas;
+
+
+/**
+ *
+ * @author T-
+ */
+public class Imc {
+    public float  calcular ( Usuario u){
+        //aqui va la logica del calculo
+        float resultado=u.peso/(u.altura*u.altura);
+        return resultado;
+    }
+}
